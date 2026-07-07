64ceb19de5327cbb639786395edb8e55b599fed0
heejun <zizibong58@gmail.com>
2026-07-07 21:58:29 +0900

    remove frontend files from backend repo

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = "https://cpabezexbhcjfvxmgavz.supabase.co"
-const SUPABASE_KEY = "sb_publishable_oB4lkE6rInHgW_wExgmyJQ_OrQe1ZxD"
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
